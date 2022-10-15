53a17049e45e82de1433e7abf59895c6f436dc1c
Eliza Weng <miki311920@gmail.com>
2022-10-14 22:39:47 -0400

    renew

--- blog-service.js
+++ blog-service.js
@@ -10,15 +10,15 @@ module.exports.initialize = function () {
         reject(err);
       } else {
         posts = JSON.parse(data);
-        resolve();
-      }
-    });
-    fs.readFile("./data/categories.json", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        categories = JSON.parse(data);
-        resolve();
+        
+        fs.readFile("./data/categories.json", "utf8", (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            categories = JSON.parse(data);
+            resolve();
+          }
+        });
       }
     });
   });
@@ -26,36 +26,87 @@ module.exports.initialize = function () {
 
 module.exports.getAllPosts = function () {
   return new Promise((resolve, reject) => {
-    if (posts.length == 0) {
-      reject("File is empty, no posts returned");
-    } else {
-      resolve(posts);
+    (posts.length > 0) ? resolve(posts): reject("no results returned");
+  });
+};
+module.exports.getPostsByCategory = function (category) {
+  return new Promise((resolve, reject) => {
+    let foundPosts = [];
+    for(let i=0; i < posts.length; i++){
+      if(posts[i].categories == category){
+        foundPosts = posts[i];
+      }
     }
+    if(!foundPosts){
+      reject("no results returned");
+    }
+    resolve(foundPosts);
+  });
+};
+module.exports.getPostsByMinDate = function (minDateStr) {
+  return new Promise((resolve, reject) => {
+    let foundPosts = [];
+    for(let i=0; i < posts.length; i++){
+      if(new Date(posts[i].postDate) >= new Date(minDateStr)){
+        foundPosts = posts[i];
+    }
+  }
+    if(!foundPosts){
+      reject("no results returned");
+    }
+    resolve(foundPosts);
+  });
+};
+module.exports.getPostById = function (id) {
+  return new Promise((resolve, reject) => {
+    let foundPosts = [];
+    for(let i=0; i < posts.length; i++){
+      if(new posts[i].id==id){
+        foundPosts = posts[i];
+    }
+  }
+    if(!foundPosts){
+      reject("no results returned");
+    }
+    resolve(foundPosts);
   });
 };
 
 module.exports.getPublishedPosts = function () {
   return new Promise((resolve, reject) => {
-    var filteredPosted = [];
-    for (let i = 0; i < posts.length; i++) {
-      if (posts[i].published == true) {
-        filteredPosted.push(posts[i]);
-      }
-    }
-    if (filteredPosted.length == 0) {
-      reject("No published posts found");
-    } else {
-      resolve(filteredPosted);
-    }
+    (posts.length > 0)? resolve(posts.filter(posts => posts.published)):reject("no results returned");   //filter: shallow copy of filtered element 
+
+    // var filteredPosted = [];
+    // for (let i = 0; i < posts.length; i++) {
+    //   if (posts[i].published == true) {
+    //     filteredPosted.push(posts[i]);
+    //   }
+    // }
+    // if (filteredPosted.length == 0) {
+    //   reject("No published posts found");
+    // } else {
+    //   resolve(filteredPosted);
+    // }
   });
 };
 
 module.exports.getCategories = function () {
   return new Promise((resolve, reject) => {
-    if (categories.length == 0) {
-      reject("File is empty, no categories returned");
-    } else {
-      resolve(categories);
-    }
+    (categories.length > 0)? resolve(categories):reject("no results returned");
+
+    // if (categories.length == 0) {
+    //   reject("File is empty, no categories returned");
+    // } else {
+    //   resolve(categories);
+    // }
+  });
+};
+
+module.exports.addPost = function (postData) {
+  return new Promise((resolve, reject) => {
+    postData.id = posts.length + 1;
+    postData.published = (postData.published)?true:false;   //checked or not
+    posts.push(postData);
+    resolve();
   });
 };
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Eliza Weng Student ID: 106800204 Date: 2022/09/30
+*  Name: Eliza Weng Student ID: 106800204 Date: 2022/10/14
 *
-*  Online (Cyclic) Link: ________________________________________________________
+*  Online (Cyclic) Link: 
 *
 ********************************************************************************/ 
 var express = require("express");
@@ -13,6 +13,16 @@ const path = require("path");
 const data = require("./blog-service.js");
 var app = express();
 var HTTP_PORT = process.env.PORT || 8080;
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+cloudinary.config({
+    cloud_name: 'dzapdrt5j',
+    api_key: '446862876139938',
+    api_secret: 'B2JYvCw62iiIvSi3zG6C1RJQOVA',
+    secure: true
+});
 
 app.use(express.static('public'));
 
@@ -30,20 +40,93 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/posts", (req, res)=>{
-    data.getAllPosts().then((data)=>{
+    if(req.query.category){
+        data.getPostsByCategory(req.query.category).then((data=>{    //then: callback function for case of promise
+            res.json(data);
+        })).catch(err=>{                    //catch: for reject case
+            res.json({message:err});
+        })
+    }else if(req.query.minDate){
+    data.getPostsByMinDate(req.query.minDate).then((data=>{    //then: callback function for case of promise
         res.json(data);
+    })).catch(err=>{                    //catch: for reject case
+        res.json({message:err});
+    })}else{
+    data.getAllPosts().then((data=>{    //then: callback function for case of promise
+        res.json(data);
+    })).catch(err=>{                    //catch: for reject case
+        res.json({message:err});
     });
+}
 });
 
 app.get("/blog", (req, res)=>{
-    data.getPublishedPosts().then((data)=>{
+    data.getPublishedPosts().then((data=>{
         res.json(data);
+    })).catch(err=>{ 
+        res.json({message:err});
     });
 });
 
 app.get("/categories", (req, res)=>{
-    data.getCategories().then((data)=>{
+    data.getCategories().then((data=>{
         res.json(data);
+    })).catch(err=>{ 
+        res.json({message:err});
+    });
+});
+
+app.get("/posts/add", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/addPost.html"));
+});
+
+app.post("/posts/add", upload.single("featureImage"), (req,res)=>{
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processPost(uploaded.url);
+        });
+    }else{
+        processPost("");
+    }
+
+    function processPost(imageUrl){
+        req.body.featureImage = imageUrl;
+
+        data.addPost(req.body).then(post=>{
+            res.redirect("/posts");
+        }).catch(err=>{
+            res.status(500).send(err);
+        })
+    }   
+});
+
+app.get("/posts/value", (req, res) => {
+    data.getPostById().then((data=>{
+        res.json(data);
+    })).catch(err=>{ 
+        res.json({message:err});
     });
 });
 
